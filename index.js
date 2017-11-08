a81e0532a0bdcd72a50daf3227bcbbc43b16d0cb
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-11-09 05:00:59 +0900

    Mock getLanguages

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const I18nJs = require('i18n-js');
 
 if (typeof RNI18n !== 'undefined') {
   I18nJs.locale = RNI18n.languages[0];
+  RNI18n.getLanguages = () => Promise.resolve(['en']);
 } else if (__DEV__) {
   console.warn('react-native-i18n module is not correctly linked');
+}
+
+if (typeof RNI18n == null) {
+  RNI18n = {
+    getLanguages: () => Promise.resolve(['en']);
+  }
 }
 
 export const getLanguages = RNI18n.getLanguages;
